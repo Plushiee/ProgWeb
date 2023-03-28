06e83665f877b7e18f52359e862bdc408c63cee9
Plushiee <barasatyaradi@gmail.com>
2023-03-28 19:30:56 +0700

    clearing

--- pertemuan7/app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	define("BASEURL", "http://localhost/pemrogramanWeb/pertemuan7/public/");
-
-	//Database
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-	define('DB_NAME', 'sia');
-?>
--- pertemuan7/app/models/UserModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-	class UserModel{
-		private $nama = 'Nikolaus Pastika Bara Satyaradi';
-	
-		public function getUser() {
-			return $this -> nama;
-		}
-	}
-?>
